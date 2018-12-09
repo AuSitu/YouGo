a0b1bdad7345b10c2f4512276aa24ff13e9d3c9e
AuSitu <454213572@qq.com>
2018-12-09 19:42:00 +0800

    弄了搜索框&轮播图的样式

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,34 +1,95 @@
 <template>
- <div>
-   首页
- </div>
+  <div class="index">
+    <!-- 顶部的 -->
+    <div class="nav">
+      <i class="iconfont icon-sousuo"></i>
+      <input
+        type="text"
+        placeholder="搜索"
+      >
+    </div>
+    <!-- 轮播图 -->
+    <swiper
+      indicator-dots
+      autoplay
+      circular
+      class="swiper"
+      indicator-active-color="#ffffff"
+      indicator-color="#557fbb"
+    >
+
+      <swiper-item
+        v-for="(item, index) in swiperList"
+        :key="index"
+      >
+        <image
+          mode="aspectFill"
+          :src="item.image_src"
+        ></image>
+      </swiper-item>
+      </block>
+    </swiper>
+
+  </div>
 </template>
 
 <script>
-
+// 导入 自己的抽取的hxios
+import hxios from "../../utils/index.js";
 
 export default {
-  data () {
+  data: function() {
     return {
-      
-  
-    }
+      // 轮播图数组
+      swiperList: []
+    };
   },
-
-  components: {
- 
-  },
-
-  methods: {
-   
-  },
-
-  created () {
-  
+  //获取数据
+  created() {
+    hxios.get("api/public/v1/home/swiperdata").then(res => {
+      // console.log(res);
+      this.swiperList = res.data.message;
+    });
   }
-}
+};
 </script>
 
-<style >
 
+
+<style lang="scss" >
+$uRed: #fe2c4b;
+.index{
+    padding-top:100rpx ;
+}
+//顶部
+.nav {
+  padding: 20rpx 16rpx;
+  background-color: $uRed;
+  box-sizing: border-box;
+  position: fixed;
+  width: 100%;
+  left: 0;
+  top: 0;
+  input {
+    background-color: #fff;
+    height: 60rpx;
+    text-align: center;
+    color: #bbb;
+    font-size: 24rpx;
+  }
+  i.icon-sousuo {
+    position: absolute;
+    color: #bbb;
+    top: 50%;
+    left: 50%;
+    transform: translate(-220%, -50%);
+  }
+}
+// 轮播图
+.swiper{
+    image{
+        width: 100%;
+    }
+}
 </style>
+
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,24 +1,25 @@
-function formatNumber (n) {
-  const str = n.toString()
-  return str[1] ? str : `0${str}`
+// 自己封装hxios.get
+
+let baseUrl = "https://autumnfish.cn/wx/"
+
+let hxios = {
+  get(url) {
+    return new Promise((resolve, rejext) => {
+      wx.request({
+        url: baseUrl + url, //开发者服务器接口地址",
+        // data: 'data', //请求的参数",
+        method: 'GET',
+        dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+        success: res => {
+          resolve(res)
+        },
+        fail: res => {
+          rejext(res)
+        },
+        complete: () => {}
+      });
+    })
+  }
 }
 
-export function formatTime (date) {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  const t1 = [year, month, day].map(formatNumber).join('/')
-  const t2 = [hour, minute, second].map(formatNumber).join(':')
-
-  return `${t1} ${t2}`
-}
-
-export default {
-  formatNumber,
-  formatTime
-}
+export default hxios;
